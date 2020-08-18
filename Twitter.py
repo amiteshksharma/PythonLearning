11a609e67b192aa0af0fd4db3fae5a9260e0f4a0
amitesh <amitesh.sharma.2001@gmail.com>
2020-08-18 16:03:06 -0700

    changes to twitter.py

--- Twitter.py
+++ Twitter.py
@@ -10,10 +10,17 @@ warnings.filterwarnings('ignore')
 nltk.download('averaged_perceptron_tagger', quiet=True)
 nltk.download('punkt', quiet=True)
 
+##################################
+#  Start of ML learning methods  #
+##################################
+
 data = {
-    'Names': ['john','jay','dan','nathan','bob'],
-    'Colors': ['yellow', 'red','green'],
-    'Places': ['tokyo','bejing','washington','mumbai'],   
+    'Names': ['john','jay','dan','nathan','bob', 'giannis', 'jokic', 'lebron', 'durant'],
+    'Politics': ['Trump', 'democrats', 'liberal', 'usps', 'progressive', 'president', 'congress', 'judicial',
+    'court', 'supreme court'],
+    'Sports': ['basketball', 'soccer', 'football', 'hockey', 'ball', 'tennis', 'racket'],
+    'Vacation': ['tokyo','bejing','washington','mumbai', 'America', 'trip', 'vacation'],
+    'Gloomy': ['sad', 'struggling', 'depression', 'suicide', 'kms', 'hate', 'sucks'],   
 }
 
 categories = {word: key for key, words in data.items() for word in words}
@@ -46,76 +53,80 @@ print(process('pink'))
 print(process('frank'))
 print(process('moscow'))
 
-# def auth():
-#     return os.environ.get("BEARER_TOKEN")
+##################################
+#  Start of Twitter API Methods  #
+##################################
 
-# def create_url():
-#     query = "Amitesh2001"
-#     tweet_fields = 10
-#     url = "https://api.twitter.com/1.1/statuses/user_timeline.json?screen_name={}&count={}".format(query, tweet_fields)
-#     return url
+def auth():
+    return os.environ.get("BEARER_TOKEN")
 
-# def create_headers(bearer_token):
-#     headers = {"Authorization": "Bearer {}".format(bearer_token)}
-#     return headers
+def create_url():
+    query = "Amitesh2001"
+    tweet_fields = 10
+    url = "https://api.twitter.com/1.1/statuses/user_timeline.json?screen_name={}&count={}".format(query, tweet_fields)
+    return url
 
-# def connect_to_endpoint(url, headers):
-#     response = requests.request("GET", url, headers=headers)
-#     print(response.status_code)
-#     if response.status_code != 200:
-#         print("There was an error")
+def create_headers(bearer_token):
+    headers = {"Authorization": "Bearer {}".format(bearer_token)}
+    return headers
+
+def connect_to_endpoint(url, headers):
+    response = requests.request("GET", url, headers=headers)
+    print(response.status_code)
+    if response.status_code != 200:
+        print("There was an error")
     
-#     print(response)
-#     return response.json()
+    print(response)
+    return response.json()
 
-# def get_response():
-#     bearer_token = auth()
-#     url = create_url()
-#     headers = create_headers(bearer_token)
-#     json_response = connect_to_endpoint(url, headers)
-#     return json_response
+def get_response():
+    bearer_token = auth()
+    url = create_url()
+    headers = create_headers(bearer_token)
+    json_response = connect_to_endpoint(url, headers)
+    return json_response
 
-# # Get the Responses from the Twitter API, which is the list of all my tweets
-# responses = get_response()
+# Get the Responses from the Twitter API, which is the list of all my tweets
+responses = get_response()
 
-# def create_dictionary():
-#     dictionary = {}
-#     for tweet in responses:
-#         string = tweet.get("text")
-#         split_string = string.split()
-#         for word in split_string:
-#             if word in dictionary:
-#                 dictionary[word] = dictionary[word] + 1
-#             else:
-#                 dictionary[word] = 1
+def create_dictionary():
+    dictionary = {}
+    for tweet in responses:
+        string = tweet.get("text")
+        split_string = string.split()
+        for word in split_string:
+            if word in dictionary:
+                dictionary[word] = dictionary[word] + 1
+            else:
+                dictionary[word] = 1
 
-#     #Sort the dictionary to get the top words
-#     sort = sorted(dictionary.items(), key=lambda x:x[1], reverse=True)
-#     # print(sort)
-#     return sort
+    #Sort the dictionary to get the top words
+    sort = sorted(dictionary.items(), key=lambda x:x[1], reverse=True)
+    # print(sort)
+    return sort
 
-# def toknize_words():
-#     # Create the sentence to tokenize and tag
-#     words = ''
-#     for tweet in responses:
-#         words += tweet["text"] + " "
+def toknize_words():
+    # Create the sentence to tokenize and tag
+    words = ''
+    for tweet in responses:
+        words += tweet["text"] + " "
         
-#     # Tokenize and then tag the text with nltk
-#     tokenize = nltk.word_tokenize(words)
-#     tagging = nltk.pos_tag(tokenize)
+    # Tokenize and then tag the text with nltk
+    tokenize = nltk.word_tokenize(words)
+    tagging = nltk.pos_tag(tokenize)
 
-#     # Create the object to count the total number of adjs, nouns, etc
-#     total_number = {}
-#     # Loop through tagging to count each type of word
-#     for token in tagging:
-#         # If it is already in the dictionary, then increment by 1
-#         if token[1] in total_number:
-#             total_number[token[1]] = total_number[token[1]] + 1
-#         # Otherwise, start the count at 1
-#         else:
-#             total_number[token[1]] = 1
-#     print(total_number)
+    # Create the object to count the total number of adjs, nouns, etc
+    total_number = {}
+    # Loop through tagging to count each type of word
+    for token in tagging:
+        # If it is already in the dictionary, then increment by 1
+        if token[1] in total_number:
+            total_number[token[1]] = total_number[token[1]] + 1
+        # Otherwise, start the count at 1
+        else:
+            total_number[token[1]] = 1
+    print(total_number)
 
-# if __name__ == "__main__":
-#     create_dictionary()
-#     toknize_words()
+if __name__ == "__main__":
+    create_dictionary()
+    toknize_words()
