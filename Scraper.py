f8ee38b04c021d159b9cd6a3d868c287dde51153
Amitesh Sharma <amitesh.sharma.2001@gmail.com>
2020-08-23 17:04:58 -0700

    machine is learning words decently, need to make names more explicit for the machine

--- Scraper.py
+++ Scraper.py
@@ -1,14 +1,91 @@
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
+import re
+import xlsxwriter
 
-driver = webdriver.Chrome()
-driver.get("https://www.englishclub.com/grammar/prepositions-list.htm")
-all_ul = driver.find_elements_by_xpath("//main/ul")
-prepositions = ''
-for item in all_ul:
-    prepositions += item.text.rstrip() + ' '
+def scrape_prepositions():
+    driver = webdriver.Chrome()
+    driver.get("https://www.englishclub.com/grammar/prepositions-list.htm")
+    all_ul = driver.find_elements_by_xpath("//main/ul")
+    prepositions = ''
+    for item in all_ul:
+        prepositions += item.text.rstrip().lower() + ' '
 
-prepositions_arr = prepositions.split()
-print(prepositions_arr)
+    prepositions_arr = prepositions.split()
+    # print(prepositions_arr)
 
-driver.close()
+    prepositions_arr.insert(0, 'Prepositions')
+    driver.close()
+    return prepositions_arr
+
+def scrape_sports():
+    driver = webdriver.Chrome()
+    driver.get("https://www.topendsports.com/sport/list/index.htm")
+    all_ul = driver.find_elements_by_xpath("//div/div/ul/li/a[1]")
+    sports = []
+    for item in all_ul:
+        sports.append(item.text.rstrip().lower())
+
+    # eliminate the random hyperlinks at the end
+    reduce_array = sports[:-15]
+    # print(reduce_array)
+    
+    reduce_array.insert(0, 'Sports')
+    driver.close()
+    return reduce_array
+
+def scrape_politics():
+    driver = webdriver.Chrome()
+    driver.get("https://myvocabulary.com/word-list/politics-vocabulary/")
+    all_ul = driver.find_elements_by_xpath("//tbody/tr/td[2]")
+    politics = []
+    for item in all_ul:
+        temp_arr = item.text.rstrip().split(' ')
+        for word in temp_arr:
+            regex = re.compile('[^a-zA-Z]')
+            word = regex.sub('', word)
+            politics.append(word.strip().lower())
+
+    politics.insert(0, 'Politics')
+    driver.close()
+    return politics
+
+def scrape_social_issues():
+    driver = webdriver.Chrome()
+    driver.get("https://www.isidewith.com/polls/popular")
+    all_ul = driver.find_elements_by_xpath("//div/div/a/div/p/span")
+    social = []
+    for item in all_ul:
+        social.append(item.text.lower())
+    
+    social.insert(0, "Social Justice")
+    driver.close()
+    return social
+
+def scrape_science():
+    driver = webdriver.Chrome()
+    driver.get("https://reversedictionary.org/wordsfor/science")
+    all_ul = driver.find_elements_by_xpath("//div/div/div[2]/div[3]/a")
+    science = []
+    for item in all_ul:
+        science.append(item.text.lower())
+    
+    science.insert(0, "Science")
+    driver.close()
+    return science
+    
+def create_spreadsheet():
+    workbook = xlsxwriter.Workbook('Words.xlsx')
+    worksheet = workbook.add_worksheet()
+
+    array = [scrape_prepositions(),
+            scrape_sports(),
+            scrape_social_issues(),
+            scrape_politics()]
+
+    row = 0
+
+    for col, data in enumerate(array):
+        worksheet.write_column(row, col, data)
+
+    workbook.close()
--- Twitter.py
+++ Twitter.py
@@ -4,6 +4,7 @@ import json
 import nltk
 from sklearn.feature_extraction.text import CountVectorizer
 from sklearn.metrics.pairwise import cosine_similarity
+from Scraper import scrape_prepositions, scrape_sports, scrape_politics, scrape_social_issues, scrape_science
 import numpy as np
 import warnings
 import re
@@ -13,20 +14,84 @@ nltk.download('averaged_perceptron_tagger', quiet=True)
 nltk.download('punkt', quiet=True)
 
 ##################################
+#  Start of Twitter API Methods  #
+##################################
+
+def auth():
+    return os.environ.get("BEARER_TOKEN")
+
+def create_headers(bearer_token):
+    headers = {"Authorization": "Bearer {}".format(bearer_token)}
+    return headers
+
+def create_url():
+    query = "Amitesh2001"
+    tweet_fields = 20
+    url = "https://api.twitter.com/1.1/statuses/user_timeline.json?screen_name={}&count={}&tweet_mode={}&trim_user={}".format(query, tweet_fields, "extended", True)
+    return url
+
+def create_url_trending():
+    url = "https://api.twitter.com/1.1/trends/place.json?id=1"
+    return url
+
+def connect_to_endpoint(url, headers):
+    response = requests.request("GET", url, headers=headers)
+    print(response.status_code)
+    if response.status_code != 200:
+        print("There was an error")
+    
+    print(response)
+    return response.json()
+
+def get_response():
+    bearer_token = auth()
+    url = create_url()
+    headers = create_headers(bearer_token)
+    json_response = connect_to_endpoint(url, headers)
+    return json_response
+
+def get_trending():
+    bearer_token = auth()
+    url = create_url_trending()
+    headers = create_headers(bearer_token)
+    json_response = connect_to_endpoint(url, headers)
+    trending_array = []
+    trends = json_response[0]['trends']
+    for name in trends:
+        try:
+            name['name'].encode(encoding='utf-8').decode('ascii')
+        except UnicodeDecodeError:
+            continue
+        else:
+            trending_array.append(name['name'].lower())
+    
+    return trending_array
+
+##################################
 #  Start of ML learning methods  #
 ##################################
 
+# 'Politics': ['senate', 'democrats', 'liberal', 'usps', 'progressive', 'president', 'congress', 'judicial',
+#     'court', 'supreme court', 'media', 'narrative', 'election', 'conservatives', 'republican', 'legislation', 'representative', 'secretary'],
+
+# Define categories to teach the machine how to categorize words
 data = {
     'Names': ['john','jay','dan','nathan','bob', 'giannis', 'jokic', 'lebron', 'durant'],
-    'Politics': ['Trump', 'democrats', 'liberal', 'usps', 'progressive', 'president', 'congress', 'judicial',
-    'court', 'supreme court', 'media', 'narrative'],
-    'Countries': ['America', 'USA', 'US', 'China', 'India', 'Honduras', 'France', 'Germany'],
-    'Sports': ['basketball', 'soccer', 'football', 'hockey', 'ball', 'tennis', 'racket'],
-    'Vacation': ['world','flying','paradise','travel', 'America', 'trip', 'vacation'],
-    'Gloomy': ['sad', 'struggling', 'depression', 'suicide', 'kms', 'hate', 'sucks'], 
+    'Politics': scrape_politics(),
+    'Athletics': scrape_sports(),
+    'Social Issues': scrape_social_issues(),
+    'Trendy': get_trending(),
+    'Science': scrape_science()
 }
 
-Pronouns = ['i', "he", 'she', 'it', 'they', 'we', 'ours', 'you']  
+Pronouns = ['i', "he", 'she', 'it', 'they', 'we', 'ours', 'you', 'a', 'an', 'the', 'im', "i'm", 'its', 'their', "it's", 'them']  
+Acronyms = ['smh', 'lol', 'lmfao', 'ttyl', 'gtfoh', 'stfu', 'rofl', 'lmk', 'ily', 'nvm', 'pov', 'rip']
+Verbs = ['are', 'were', 'will', 'is', 'was', 'have', 'has', 'had', 'is']
+Questions = ['who', 'what', 'when', 'where', 'why', 'how']
+Conjunctions = ['for', 'and', 'nor', 'but', 'or', 'yet', 'so']
+
+
+Prepositions = scrape_prepositions()
 
 categories = {word: key for key, words in data.items() for word in words}
 
@@ -56,37 +121,8 @@ def process(query):
     return scores
 
 ##################################
-#  Start of Twitter API Methods  #
+#  Start of ML learning process  #
 ##################################
-
-def auth():
-    return os.environ.get("BEARER_TOKEN")
-
-def create_url():
-    query = "Amitesh2001"
-    tweet_fields = 20
-    url = "https://api.twitter.com/1.1/statuses/user_timeline.json?screen_name={}&count={}&tweet_mode={}&trim_user={}".format(query, tweet_fields, "extended", True)
-    return url
-
-def create_headers(bearer_token):
-    headers = {"Authorization": "Bearer {}".format(bearer_token)}
-    return headers
-
-def connect_to_endpoint(url, headers):
-    response = requests.request("GET", url, headers=headers)
-    print(response.status_code)
-    if response.status_code != 200:
-        print("There was an error")
-    
-    print(response)
-    return response.json()
-
-def get_response():
-    bearer_token = auth()
-    url = create_url()
-    headers = create_headers(bearer_token)
-    json_response = connect_to_endpoint(url, headers)
-    return json_response
 
 # Get the Responses from the Twitter API, which is the list of all my tweets
 responses = get_response()
@@ -104,7 +140,7 @@ def create_dictionary():
 
     #Sort the dictionary to get the top words
     sort = sorted(dictionary.items(), key=lambda x:x[1], reverse=True)
-    print(sort)
+    # print(sort)
     return sort
 
 def tokenize_words():
@@ -127,27 +163,37 @@ def tokenize_words():
         # Otherwise, start the count at 1
         else:
             total_number[token[1]] = 1
-    print(total_number)
+    # print(total_number)
 
 def categorize_words():
     word_dict = {}
+    data = {
+        'Names': [],
+        'Politics': [],
+        'Athletics': [],
+        'Social Issues': [],
+        'Trendy': [],
+        'Science': []
+    }
+
     for tweet in responses:
         word = tweet.get('full_text')
-        sort_word_into_category(word, word_dict)
+        sort_word_into_category(word, word_dict, data)
 
         if "quoted_status" in tweet:
             retweet = tweet.get('quoted_status')['full_text']
-            sort_word_into_category(retweet, word_dict)
+            sort_word_into_category(retweet, word_dict, data)
  
+    print(data)
     return word_dict
 
-def sort_word_into_category(word, word_dict):
+def sort_word_into_category(word, word_dict, data):
     arr_words = word.split()
     for words in arr_words:
-        if words.lower() in Pronouns:
-            continue
-
-        if "https" in words:
+        words = words.lower()
+        if words in Pronouns or words in Prepositions or \
+        words in Acronyms or words in Verbs or "https" in words \
+        or words in Questions or words in Conjunctions:
             continue
         
         regex = re.compile('[^a-zA-Z]')
@@ -164,6 +210,8 @@ def sort_word_into_category(word, word_dict):
                 word_dict[largest_value] = word_dict[largest_value] + 1
             else:
                 word_dict[largest_value] = 1
+            
+            data[largest_value].append(words)
             
 
 if __name__ == "__main__":
